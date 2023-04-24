263bd6ba9360fde0db46235f24e6e2dcb2b03142
andrebemfica <andrecbemfica@gmail.com>
2023-04-24 09:45:37 -0300

    check point

--- src/main/java/com/bemfis/algafoodapi/api/controller/RestauranteCtrl.java
+++ src/main/java/com/bemfis/algafoodapi/api/controller/RestauranteCtrl.java
@@ -1,14 +1,14 @@
 package com.bemfis.algafoodapi.api.controller;
 
+import com.bemfis.algafoodapi.domain.exception.EntidadeNaoEncontradaException;
 import com.bemfis.algafoodapi.domain.model.Restaurante;
 import com.bemfis.algafoodapi.domain.service.CadastroRestauranteService;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -17,16 +17,43 @@ import java.util.List;
 public class RestauranteCtrl {
     @Autowired
     private CadastroRestauranteService cadastroRestauranteService;
+
     @GetMapping
-    public List<Restaurante> listar(){
+    public List<Restaurante> listar() {
         return cadastroRestauranteService.listar();
     }
+
     @GetMapping("/{restauranteId}")
-    public ResponseEntity<Restaurante> buscar(@PathVariable Long restauranteId){
+    public ResponseEntity<Restaurante> buscar(@PathVariable Long restauranteId) {
         Restaurante restaurante = cadastroRestauranteService.buscar(restauranteId);
-        if(restaurante != null){
+        if (restaurante != null) {
             return ResponseEntity.ok(restaurante);
         }
         return ResponseEntity.notFound().build();
+    }
+
+    @PostMapping
+    public ResponseEntity<?> adicionar(@RequestBody Restaurante restaurante) {
+        try {
+            Restaurante restauranteSalvo = cadastroRestauranteService.salvar(restaurante);
+            return ResponseEntity.status(HttpStatus.CREATED).body(restauranteSalvo);
+        } catch (EntidadeNaoEncontradaException e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
+
+    @PutMapping("/{restauranteId}")
+    public ResponseEntity<?> atualizar(@RequestBody Restaurante restaurante, @PathVariable Long restauranteId) {
+        try {
+            Restaurante restauranteAtual = cadastroRestauranteService.buscar(restauranteId);
+            if (restauranteAtual == null) {
+                return ResponseEntity.notFound().build();
+            }
+            BeanUtils.copyProperties(restaurante, restauranteAtual, "id");
+            Restaurante restauranteAtualizado = cadastroRestauranteService.salvar(restauranteAtual);
+            return ResponseEntity.ok(restauranteAtualizado);
+        } catch (EntidadeNaoEncontradaException e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
     }
 }
--- src/main/java/com/bemfis/algafoodapi/domain/service/CadastroRestauranteService.java
+++ src/main/java/com/bemfis/algafoodapi/domain/service/CadastroRestauranteService.java
@@ -1,20 +1,36 @@
 package com.bemfis.algafoodapi.domain.service;
 
+import com.bemfis.algafoodapi.domain.exception.EntidadeNaoEncontradaException;
+import com.bemfis.algafoodapi.domain.model.Cozinha;
 import com.bemfis.algafoodapi.domain.model.Restaurante;
+import com.bemfis.algafoodapi.domain.repository.CozinhaRepository;
 import com.bemfis.algafoodapi.domain.repository.RestauranteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+
 @Service
 public class CadastroRestauranteService {
     @Autowired
     private RestauranteRepository restauranteRepository;
-    public List<Restaurante> listar(){
+    @Autowired
+    private CozinhaRepository cozinhaRepository;
+
+    public List<Restaurante> listar() {
         return restauranteRepository.listar();
     }
-    public Restaurante buscar(Long restauranteId){
+
+    public Restaurante buscar(Long restauranteId) {
         return restauranteRepository.buscar(restauranteId);
     }
 
+    public Restaurante salvar(Restaurante restaurante) {
+        Cozinha cozinha = cozinhaRepository.buscar(restaurante.getCozinha().getId());
+        if (cozinha == null) {
+            throw new EntidadeNaoEncontradaException(String.format(
+                    "Não existe um cadastro de cozinha com o código %d", restaurante.getCozinha().getId()));
+        }
+        return restauranteRepository.salvar(restaurante);
+    }
 }
