3175edad2ec5a9336b0e467e5f8aa78292a36ca4
bemfica <andrecbemfica@gmail.com>
2023-07-05 11:51:28 -0300

    Implementando um repositório SDJ customizado

--- src/main/java/com/bemfis/algafoodapi/api/controller/expEstudo/TesteController.java
+++ src/main/java/com/bemfis/algafoodapi/api/controller/expEstudo/TesteController.java
@@ -39,6 +39,11 @@ public class TesteController {
         return restauranteRepository.findByTaxaFreteBetween(taxaInicial, taxaFinal);
     }
 
+    @GetMapping("/restaurantes/por-nome-e-frete")
+    public List<Restaurante> restaurantesPorNomeFrete(String nome, BigDecimal taxaFreteInicial, BigDecimal taxaFreteFinal) {
+        return restauranteRepository.find(nome, taxaFreteInicial, taxaFreteFinal);
+    }
+
     @GetMapping("/restaurantes/por-nome-cozinhaid")
     public List<Restaurante> restaurantesPorNomeCozinhaId(String nome, Long cozinhaId) {
         return restauranteRepository.consultarPorNome(nome, cozinhaId);
---
+++ src/main/java/com/bemfis/algafoodapi/domain/repository/RestauranteRepositoryQueries.java
@@ -0,0 +1,10 @@
+package com.bemfis.algafoodapi.domain.repository;
+
+import com.bemfis.algafoodapi.domain.model.Restaurante;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+public interface RestauranteRepositoryQueries {
+    List<Restaurante> find(String nome, BigDecimal taxaFreteInicial, BigDecimal taxaFreteFinal);
+}
