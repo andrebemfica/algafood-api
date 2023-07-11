e1917cad5258cb1c25d2d6ddd2c770fa3c553e9b
bemfica <andrecbemfica@gmail.com>
2023-07-11 17:25:39 -0300

    Implementando uma consulta simples com Criteria API

--- src/main/java/com/bemfis/algafoodapi/infrastructure/repository/RestauranteRepositoryImpl.java
+++ src/main/java/com/bemfis/algafoodapi/infrastructure/repository/RestauranteRepositoryImpl.java
@@ -5,6 +5,8 @@ import com.bemfis.algafoodapi.domain.repository.RestauranteRepositoryQueries;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
 import jakarta.persistence.TypedQuery;
+import jakarta.persistence.criteria.CriteriaBuilder;
+import jakarta.persistence.criteria.CriteriaQuery;
 import org.springframework.stereotype.Repository;
 import org.springframework.util.StringUtils;
 
@@ -20,7 +22,7 @@ public class RestauranteRepositoryImpl implements RestauranteRepositoryQueries {
 
     @Override
     public List<Restaurante> find(String nome, BigDecimal taxaFreteInicial, BigDecimal taxaFreteFinal){
-
+        /*
         var jpql = new StringBuilder();
         jpql.append("from Restaurante where 0 = 0 ");
 
@@ -44,6 +46,15 @@ public class RestauranteRepositoryImpl implements RestauranteRepositoryQueries {
         TypedQuery<Restaurante> query = manager.createQuery(jpql.toString(), Restaurante.class);
 
         parametros.forEach((chave, valor) -> query.setParameter(chave, valor));
+
+        return query.getResultList();*/
+
+        CriteriaBuilder builder = manager.getCriteriaBuilder(); //fábrica para construir elementos para uma consulta
+
+        CriteriaQuery<Restaurante> criteria = builder.createQuery(Restaurante.class); //monta a estrutura de uma query, no caso uma consulta de restaurante
+        criteria.from(Restaurante.class);                                            //construtor de cláusulas
+
+        TypedQuery<Restaurante> query = manager.createQuery(criteria);
 
         return query.getResultList();
     }
