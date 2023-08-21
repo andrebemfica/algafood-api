6be2e47a7c1666948105c66740b50c96a79c7d76
andrebemfica <andrecbemfica@gmail.com>
2023-08-21 15:05:24 -0300

    Tornando a consulta com Criteria API com filtros dinâmicos

--- src/main/java/com/bemfis/algafoodapi/infrastructure/repository/RestauranteRepositoryImpl.java
+++ src/main/java/com/bemfis/algafoodapi/infrastructure/repository/RestauranteRepositoryImpl.java
@@ -7,10 +7,13 @@ import jakarta.persistence.PersistenceContext;
 import jakarta.persistence.TypedQuery;
 import jakarta.persistence.criteria.CriteriaBuilder;
 import jakarta.persistence.criteria.CriteriaQuery;
+import jakarta.persistence.criteria.Predicate;
+import jakarta.persistence.criteria.Root;
 import org.springframework.stereotype.Repository;
 import org.springframework.util.StringUtils;
 
 import java.math.BigDecimal;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
@@ -51,8 +54,24 @@ public class RestauranteRepositoryImpl implements RestauranteRepositoryQueries {
 
         CriteriaBuilder builder = manager.getCriteriaBuilder(); //fábrica para construir elementos para uma consulta
 
+        //construtor de cláusulas
         CriteriaQuery<Restaurante> criteria = builder.createQuery(Restaurante.class); //monta a estrutura de uma query, no caso uma consulta de restaurante
-        criteria.from(Restaurante.class);                                            //construtor de cláusulas
+        Root<Restaurante> root = criteria.from(Restaurante.class);
+
+        var predicates = new ArrayList<Predicate>();
+        if(StringUtils.hasText(nome)) {
+            predicates.add(builder.like(root.get("nome"), "%" + nome + "%"));
+        }
+
+        if(taxaFreteInicial != null) {
+            predicates.add(builder.greaterThanOrEqualTo(root.get("taxaFrete"), taxaFreteInicial));
+        }
+
+        if(taxaFreteFinal != null) {
+            predicates.add(builder.lessThanOrEqualTo(root.get("taxaFrete"), taxaFreteFinal));
+        }
+
+        criteria.where(predicates.toArray(new Predicate[0]));
 
         TypedQuery<Restaurante> query = manager.createQuery(criteria);
 
