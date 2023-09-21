700bfb82edec9b9be8ef3adccc8b76c3829db70b
andrebemfica <andrecbemfica@gmail.com>
2023-09-21 16:25:00 -0300

    Mapeando relacionamento muitos-para-muitos com @ManyToMany

--- src/main/java/com/bemfis/algafoodapi/domain/model/Restaurante.java
+++ src/main/java/com/bemfis/algafoodapi/domain/model/Restaurante.java
@@ -6,6 +6,8 @@ import lombok.Data;
 import lombok.EqualsAndHashCode;
 
 import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
 
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
 @Data
@@ -25,4 +27,9 @@ public class Restaurante {
     @ManyToOne
     @JoinColumn(name = "cozinha_id", nullable = false)
     private Cozinha cozinha;
+
+    @ManyToMany
+    @JoinTable(name = "restaurante_forma_pagamento", joinColumns = @JoinColumn(name = "restaurante_id"),
+            inverseJoinColumns = @JoinColumn(name = "forma_pagamento_id"))
+    private List<FormaPagamento> formaPagamentos = new ArrayList<>();
 }
--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -18,3 +18,5 @@ insert into estado (nome) values ('Amapa');
 insert into cidade (nome, estado_id) values ('Monte Dourado', 1);
 insert into cidade (nome, estado_id) values ('Belem', 1);
 insert into cidade (nome, estado_id) values ('Macapa', 2);
+
+insert into restaurante_forma_pagamento (restaurante_id, forma_pagamento_id) values (1, 1), (1, 2), (1, 3), (2, 3), (3, 2), (3, 3);
